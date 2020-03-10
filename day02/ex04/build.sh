32edb231e344f08dccb8d3f5e2e230ca4652e523
cclaude42 <cesarclaudepro@gmail.com>
2020-03-10 15:21:49 +0100

    finished day02

--- day02/ex04/build.sh
+++ day02/ex04/build.sh
@@ -1,3 +1,3 @@
-# Run : bash build.sh && pip install ./dist/ai42-1.0.0.tar.gz
+# Run : bash build.sh && pip install --user ./dist/ai42-1.0.0.tar.gz
 python setup.py sdist
 # Check with : pip list
--- day02/ex04/setup.py
+++ day02/ex04/setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 setup(name='ai42',
       version='1.0.0',
-      py_modules=['logging', 'progressbar'],
+      description='Custom Python library',
+      author='cclaude',
       )
