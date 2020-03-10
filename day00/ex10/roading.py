383f7f1362d478055458e9d39fbc8756f7d6b7d3
cclaude42 <cesarclaudepro@gmail.com>
2020-03-10 12:57:15 +0100

    reviewed everything, added a few modifications

--- day00/ex10/roading.py
+++ day00/ex10/roading.py
@@ -25,11 +25,11 @@ def ft_progress(list):
         sys.stdout.flush()
         yield i
 
-
-listy = range(1001)
-ret = 0
-for elem in ft_progress(listy):
-    ret += (elem + 3) % 5
-    time.sleep(0.01)
-print()
-print(ret)
+if __name__ == "__main__":
+    listy = range(1001)
+    ret = 0
+    for elem in ft_progress(listy):
+        ret += (elem + 3) % 5
+        time.sleep(0.01)
+    print()
+    print(ret)
--- day02/ex03/csvreader.py
+++ day02/ex03/csvreader.py
@@ -1,23 +1,51 @@
-
-
-
 class CsvReader():
-    def __init__(self, file_name, sep=',', header=False, skip_top=0, skip_bottom=0):
-        self.file_obj = open(file_name, 'r')
+	def __init__(self, file_name, sep=',', header=False, skip_top=0, skip_bottom=0):
+		self.name = file_name
+		self.sep = sep
+		self.hbool = header
+		self.top = skip_top
+		self.bot = skip_bottom
 
-    def __enter__(self):
-        return self.file_obj
+	def __enter__(self):
+		try:
+			self.file_obj = open(self.name, 'r')
+		except:
+			return None
+		lst = self.file_obj.read().split('\n')
+		# print(lst)
+		if self.hbool:
+			self.header = lst[0].split(self.sep)
+			lst = lst[1:]
+		else:
+			self.header = "[ No header ]"
+		lst = lst[self.top : len(lst) - self.bot - 1]
+		self.data = [i.split(self.sep) for i in lst]
+		return self
 
-    def __exit__(self, *args):
-        self.file_obj.close()
+	def __exit__(self, *args):
+		if hasattr(self, "file"):
+			self.file_obj.close()
 
-    def getdata(self):
-        pass
+	def getheader(self):
+		return self.header
 
-    def getheader(self):
-        pass
+	def getdata(self):
+		return self.data
 
-if __name__ == "__main__":
-    with CsvReader('good.csv') as file:
-        data = file.getdata()
-        header = file.getheader()
+print("=== Good ===")
+with CsvReader("good.csv", ',', False) as reader:
+	if reader == None:
+		print("File corrupted!")
+	else:
+		print(reader.getheader())
+		for val in reader.getdata():
+			print(val)
+
+print("\n=== Bad ===")
+with CsvReader("bad.csv", ',', True) as reader:
+	if reader == None:
+		print("File corrupted!")
+	else:
+		print(reader.getheader())
+		for val in reader.getdata():
+			print(val)
